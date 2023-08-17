aa62c29551ff79a816524791e76a719c0eddf58a
Somrat-Masud <somratmasud098@gmail.com>
2023-08-17 17:43:05 +0600

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 let total = 0;
+function  handleClikBtn(target){
+    const selecteditmes = document.getElementById('selected-itmes');
+const itemName = target.parentNode.childNodes[3].innerText;
+const li = document.createElement("li");
+li.innerText =itemName;
+document.getElementById('selected-itmes').innerText;
+selecteditmes.appendChild(li);
+const parice = (target.parentNode.childNodes[7].innerText.split(" ")[1]);
 
-function handleCLikBtn(target) {
-  const selectedItemContainer = document.getElementById("selected-items");
-  const itemName = target.parentNode.childNodes[1].innerText;
-  const li = document.createElement("li");
-  li.innerText = itemName;
-  selectedItemContainer.appendChild(li);
-  const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
-  total = parseInt(total) + parseInt(price);
-  document.getElementById("total").innerText = total;
+total = parseInt(total) + parseInt (parice);
+
+document.getElementById('total').innerText = total;
 }
-
-// document.getElementById("").addEventListener("click", function (e) {
-//     e.target
-// })
