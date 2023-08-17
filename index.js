23016122d3ffb02c8236402ae7c483e89f1b0715
Somrat-Masud <somratmasud098@gmail.com>
2023-08-17 21:53:24 +0600

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let total = 0;
-function  handleClikBtn(target){
-    const selecteditmes = document.getElementById('selected-itmes');
+function  handleClickBtn(target){
+    const selectedItem = document.getElementById('selected-item');
 const itemName = target.parentNode.childNodes[3].innerText;
 const li = document.createElement("li");
 li.innerText =itemName;
-document.getElementById('selected-itmes').innerText;
-selecteditmes.appendChild(li);
-const parice = (target.parentNode.childNodes[7].innerText.split(" ")[1]);
+document.getElementById('selected-item').innerText;
+selectedItem.appendChild(li);
+const Price = (target.parentNode.childNodes[7].innerText.split(" ")[1]);
 
-total = parseInt(total) + parseInt (parice);
+total = parseInt(total) + parseInt (Price);
 
 document.getElementById('total').innerText = total;
 }
